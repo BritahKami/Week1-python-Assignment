dc3d4bf78a1c3d15b79f3c2f6cf8c36b48d1c22b
BritahKami <mukamibridget7@gmail.com>
2025-03-26 23:48:53 +0300

    Week1 Assignment

---
+++ calculator.py
@@ -0,0 +1,9 @@
+num1= input("Enter a number:")
+num2= input("Enter another number:")
+
+num3= input("Enter an arithmetic operation:")
+
+result= eval(num1+ num3+ num2)
+
+print(num1, num3, num2, "=", result)
+# End of code
